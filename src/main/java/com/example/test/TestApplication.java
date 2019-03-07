484c70ef32fbf475a1fbd941daf7bd60875ce20a
sun-xch <13050571762@163.com>
2019-03-07 16:53:33 +0800

    quartz demo

--- src/main/java/com/example/test/TestApplication.java
+++ src/main/java/com/example/test/TestApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
-@ComponentScan("com.example.test")
+@ComponentScan
 @MapperScan("com.example.test.quartz.dao")
 @SpringBootApplication
 public class TestApplication {
